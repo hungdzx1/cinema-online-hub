054f5aa0312a3351ae4741e2d8511b669a39a361
hung123-alt <loct63636@gmail.com>
2026-06-14 17:36:07 +0700

    Update schema.sql

--- server/src/database/schema.sql
+++ server/src/database/schema.sql
@@ -15,21 +15,19 @@ CREATE TABLE IF NOT EXISTS movies (
 );
 
 // Hùng
-CREATE TABLE IF NOT EXISTS episodes (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    movie_id ID REFERENCES movies(id) ON DELETE CASCADE,
-    episode_number int not null,
-    title varchar(255) NULL,
-    embed_url varchar(500) not null,
-    sever_name varchar(100) null default 'Sever 1';
-    skip_intro_seconds int not null default 0,
-    duration_seconds int not null default 0,
-    view_count INT not null DEFAULT 0,
-    created_at DATETIME not null default CURRENT_TIMESTAMP,
-    updated_at DATETIME not null default CURRENT_TIMESTAMP on update  CURRENT_TIMESTAMP ,
-    CONSTRAINT fk_episode_movie foreign key (movie_id) references movie(id) on ON DELETE CASCADE,
-  UNIQUE KEY uq_episode (movie_id, episode_number, server_name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS users (
+    id VARCHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    full_name VARCHAR(100),
+    avatar_url VARCHAR(255),
+    role VARCHAR(20) DEFAULT 'USER',
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CHECK (role IN ('USER', 'ADMIN'))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci;
 
 
 INSERT INTO episodes (id, movie_id, episode_number, title, embed_url, server_name, duration_seconds) VALUES
@@ -38,6 +36,6 @@ INSERT INTO episodes (id, movie_id, episode_number, title, embed_url, server_nam
 ('e3333333-3333-3333-3333-333333333333', 'm2222222-2222-2222-2222-222222222222', 2, 'Tập 2: Kẻ lạ mặt', 'https://player.com/st-ep2', 'Server 1', 3500);
 
 
-INSERT INTO movies (id, title, slug, description, poster_url, backdrop_url, trailer_url, type, status, release_year, country_id) VALUES
-('m1111111-1111-1111-1111-111111111111', 'Mai', 'mai-2024', 'Phim điện ảnh tình cảm gia đình...', 'mai-poster.jpg', 'mai-bg.jpg', 'youtube.com/mai', 'SINGLE', 'COMPLETED', 2024, 1),
-('m2222222-2222-2222-2222-222222222222', 'Stranger Things', 'stranger-things', 'Một nhóm bạn trẻ khám phá thế giới song song...', 'st-poster.jpg', 'st-bg.jpg', 'youtube.com/st', 'SERIES', 'ONGOING', 2022, 2);
+INSERT INTO users (id, email, password_hash, full_name, avatar_url, role) VALUES
+('u1111111-1111-1111-1111-111111111111', 'admin@phimhay.com', 'hashed_password_123', 'Quản Trị Viên', 'https://domain.com/admin.jpg', 'ADMIN'),
+('u2222222-2222-2222-2222-222222222222', 'khachhang@gmail.com', 'hashed_password_456', 'Nguyễn Văn User', 'https://domain.com/user.jpg', 'USER');
