1a3ef19ff905749d27175aebacd8a827a13176dc
hung123-alt <loct63636@gmail.com>
2026-06-21 22:00:26 +0700

    Update movies.module.ts

--- server/src/modules/movies/movies.module.ts
+++ server/src/modules/movies/movies.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
 import { Movie } from './movies.entity';
+import { Genre } from '../genres/genre.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Movie])],
+  imports: [TypeOrmModule.forFeature([Movie, Genre])],
   controllers: [MoviesController],
   providers: [MoviesService],
   exports: [MoviesService],
