6cee031f782b903880db567a9ba8733b26db77c4
hung123-alt <loct63636@gmail.com>
2026-06-22 23:58:48 +0700

    Update countries.module.ts

--- server/src/modules/countries/countries.module.ts
+++ server/src/modules/countries/countries.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CountriesService } from './countries.service';
 import { CountriesController } from './countries.controller';
-import { Country } from './country.entity';
+import { Country } from './countries.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Country])],
